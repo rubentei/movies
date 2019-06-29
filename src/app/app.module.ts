f4347d9a086e39e2f57bbd3a4fea8037899a5ff0
RubenTei <rubenmenendezgarcia@gmail.com>
2019-06-29 20:50:11 +0200

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
 import { UserComponent } from './user/user.component';
-import { StartComponent } from './start/start.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesComponent } from './movies/movies.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MovieAdderComponent } from './movie-adder/movie-adder.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MoviesearchComponent } from './moviesearch/moviesearch.component';
 import { RatePipe } from './rate.pipe';
@@ -18,12 +15,9 @@ import { RatePipe } from './rate.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstComponentComponent,
     UserComponent,
-    StartComponent,
     MovieDetailsComponent,
     MoviesComponent,
-    MovieAdderComponent,
     MoviesearchComponent,
     RatePipe
   ],
